4513bd47bdfcb07dd8015d4b2749d1608925ed7b
bckim9489 <bckim9489@gmail.com>
2023-12-29 14:48:21 +0900

    first Commit

---
+++ jungle/src/test/java/com/jungle/jungleWeb/JungleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jungle.jungleWeb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JungleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
