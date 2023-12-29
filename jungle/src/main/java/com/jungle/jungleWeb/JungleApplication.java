a0b90ffa42622b57f96a7de7254f7fe581797105
bckim9489 <bckim9489@gmail.com>
2023-12-29 16:32:26 +0900

    "react 연동"

--- jungle/src/main/java/com/jungle/jungleWeb/JungleApplication.java
+++ jungle/src/main/java/com/jungle/jungleWeb/JungleApplication.java
@@ -1,4 +1,4 @@
-package com.jungle.jungleWeb;
+package com.jungle.jungleweb;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class JungleApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(JungleApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(JungleApplication.class, args);
+    }
 
 }
--- jungle/src/test/java/com/jungle/jungleWeb/JungleApplicationTests.java
+++ jungle/src/test/java/com/jungle/jungleWeb/JungleApplicationTests.java
@@ -1,4 +1,4 @@
-package com.jungle.jungleWeb;
+package com.jungle.jungleweb;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class JungleApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
