239038d3e3adf21e78a3e3347f1722c3fd91a386
bckim9489 <bckim9489@gmail.com>
2023-12-30 12:13:17 +0900

    DB 연결 완

--- jungle/src/main/java/com/jungle/jungleWeb/controller/MainController.java
+++ jungle/src/main/java/com/jungle/jungleWeb/controller/MainController.java
@@ -1,13 +1,35 @@
 package com.jungle.jungleweb.controller;
 
+import com.jungle.jungleweb.domain.entity.User;
+import com.jungle.jungleweb.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
+@RequiredArgsConstructor
+@RequestMapping("/")
 public class MainController {
+    private UserService userService;
+
+    @Autowired
+    public MainController(UserService userService) {
+        this.userService = userService;
+    }
+
     @GetMapping("/api/test")
     public String hello(){
         return "테스트입니다.";
+    }
+
+    @GetMapping("/api/all")
+    public List<User> getUserList(){
+
+        return userService.findAll();
     }
 
 }
