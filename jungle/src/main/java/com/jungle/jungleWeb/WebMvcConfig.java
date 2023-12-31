c17e156b43ea61c1e4246804592b0f9c7e0b98a3
bckim9489 <bckim9489@gmail.com>
2023-12-31 15:47:04 +0900

    개발, 배포 profile 생성, 배포자동화 설정

--- jungle/src/main/java/com/jungle/jungleWeb/WebMvcConfig.java
+++ jungle/src/main/java/com/jungle/jungleWeb/WebMvcConfig.java
@@ -1,5 +1,6 @@
 package com.jungle.jungleweb;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
@@ -8,11 +9,14 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 @EnableWebMvc
 public class WebMvcConfig implements WebMvcConfigurer {
+
+    @Value("${spring.cors.allowed-origin:http://localhost:3000}")
+    private String allowedOrigin;
     @Override
     public void addCorsMappings(CorsRegistry registry){
         //로컬
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000")
+                .allowedOrigins(allowedOrigin)
                 .allowedMethods("OPTIONS", "GET", "POST", "PUT", "DELETE");
     }
 }
